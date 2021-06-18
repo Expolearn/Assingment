e0ff2f6f3e703fa207b1d6f355c612bcb7ed2b13
Murexmurendeni <2021007@expotechindustries.com>
2021-06-19 00:57:00 +0200

    gender
    me

--- mures.js
+++ mures.js
@@ -30,6 +30,33 @@ readline.question('what is your age?', age => {
 
         console.log('allowed');
 
+
+
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        readline.question('what is your age?', age => {
+
+
+            if (age < 19) {
+
+                console.log('NotAllowed');
+
+            } else if (age >= 20 && age < 100) {
+
+                console.log('allowed');
+                console.log('your a female');
+
+            } else {
+
+                console.log('Too old');
+            }
+
+            readline.close();
+        });
+
     } else {
 
         console.log('Too old');
